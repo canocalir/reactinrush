a4672e5e32006a0c72658e8805e57ba50e9ebdce
Can Berk Ocalir <can.ocalir@gmail.com>
2022-10-10 15:41:19 +0300

    bundle folder changed

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
   resolve: {
