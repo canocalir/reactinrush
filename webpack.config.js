67c3b0ec618782e064ae4c30d35eea6a1dc3bb4e
Can Berk Ocalir <can.ocalir@gmail.com>
2022-10-10 15:49:21 +0300

    fixed build folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     filename: "main.js",
   },
   resolve: {
